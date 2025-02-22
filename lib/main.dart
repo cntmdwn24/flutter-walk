8dde6c0e30aed32ba72f235aef80645079a2f2aa
cntmdwn24 <cntmdwn5522@gmail.com>
2025-02-22 14:07:32 +0900

    02/22
    flutter_native_splash 패키지 설치

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:walk/presentation/screens/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+final supportedLocales = [Locale('en', 'US'), Locale('ko', 'KR')];
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  Future.delayed(Duration(seconds: 1), () {
+    runApp(
+      EasyLocalization(
+          supportedLocales: supportedLocales,
+          path: 'assets/translations',
+          fallbackLocale: Locale('en', 'US'),
+          child: MyApp()),
+    );
+  });
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      home: HomeScreen(),
-    );
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        home: HomeScreen());
   }
 }
