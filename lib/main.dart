19e13bb0eae8278ddc353ccb60626d0a3b360cd9
cntmdwn24 <cntmdwn5522@gmail.com>
2025-02-22 13:39:17 +0900

    02/22

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:walk/presentation/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: Text('data'),
+      home: HomeScreen(),
     );
   }
 }
