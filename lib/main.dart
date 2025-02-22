1c2184598cf9f0ecd5e30b9158a13135650d1e1a
cntmdwn24 <cntmdwn5522@gmail.com>
2025-02-22 14:47:04 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
+        theme: ThemeData(fontFamily: 'NanumSquare'),
         localizationsDelegates: context.localizationDelegates,
         supportedLocales: context.supportedLocales,
         locale: context.locale,
