d0b849b06a81b2945320271ea08c42b24343ccaa
cntmdwn24 <cntmdwn5522@gmail.com>
2025-02-24 12:46:45 +0900

    02/24

---
+++ lib/presentation/screens/allow_alarm_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AllowAlarmScreen extends StatelessWidget {
+  const AllowAlarmScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
